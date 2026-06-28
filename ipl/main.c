4f69869b6ec389e1cf9e42391e76ad6849e5b276
Daniel Palmer <daniel@thingy.jp>
2026-06-29 07:51:39 +0900

    tmp

--- ipl/main.c
+++ ipl/main.c
@@ -8,26 +8,9 @@
 
 static struct ipl_params ipl_params = { 0 };
 
-/* See: https://wiki.osdev.org/RSDP */
-struct XSDP {
-	char signature[8];
-	uint8_t checksum;
-	char oemid[6];
-	uint8_t revision;
-	uint32_t rsdtaddress;
-
-	uint32_t length;
-	uint64_t xsdtAddress;
-	uint8_t extendedchecksum;
-	uint8_t reserved[3];
-} __attribute__ ((packed));
-
-static struct XSDP rsdp = {
-};
-
 /* --- ACPI tables --------------------------------------------------------- */
 
-/* Common header on every ACPI system description table (the "SDT" header) */
+/* Common header on every ACPI system description table */
 struct acpi_sdt_header {
 	char signature[4];
 	uint32_t length;
@@ -38,6 +21,15 @@ struct acpi_sdt_header {
 	uint32_t oem_revision;
 	uint32_t creator_id;
 	uint32_t creator_revision;
+} __attribute__ ((packed));
+
+/* Generic Address Structure (used by the FADT) */
+struct acpi_gas {
+	uint8_t space_id;
+	uint8_t bit_width;
+	uint8_t bit_offset;
+	uint8_t access_size;
+	uint64_t address;
 } __attribute__ ((packed));
 
 /* MADT entry types we emit */
@@ -63,8 +55,8 @@ struct acpi_madt_interrupt_override {
 	uint8_t length;			/* 10 */
 	uint8_t bus;			/* 0 = ISA */
 	uint8_t source;			/* source IRQ */
-	uint32_t gsi;			/* global system interrupt it maps to */
-	uint16_t flags;			/* polarity / trigger (0 = bus default) */
+	uint32_t gsi;
+	uint16_t flags;			/* 0 = bus default polarity/trigger */
 } __attribute__ ((packed));
 
 struct ipl_madt {
@@ -76,54 +68,180 @@ struct ipl_madt {
 	struct acpi_madt_interrupt_override pit_override;
 } __attribute__ ((packed));
 
+struct ipl_dsdt {
+	struct acpi_sdt_header header;
+	uint8_t aml[7];			/* Scope(\_SB){} */
+} __attribute__ ((packed));
+
+#define ACPI_FADT_HW_REDUCED	(1u << 20)	/* no FACS / PM register blocks */
+#define ACPI_IAPC_NO_VGA	(1u << 2)
+#define ACPI_IAPC_NO_MSI	(1u << 3)
+
+struct ipl_fadt {
+	struct acpi_sdt_header header;
+	uint32_t firmware_ctrl;
+	uint32_t dsdt;
+	uint8_t reserved0;
+	uint8_t preferred_pm_profile;
+	uint16_t sci_int;
+	uint32_t smi_cmd;
+	uint8_t acpi_enable;
+	uint8_t acpi_disable;
+	uint8_t s4bios_req;
+	uint8_t pstate_cnt;
+	uint32_t pm1a_evt_blk, pm1b_evt_blk, pm1a_cnt_blk, pm1b_cnt_blk;
+	uint32_t pm2_cnt_blk, pm_tmr_blk, gpe0_blk, gpe1_blk;
+	uint8_t pm1_evt_len, pm1_cnt_len, pm2_cnt_len, pm_tmr_len;
+	uint8_t gpe0_blk_len, gpe1_blk_len, gpe1_base, cst_cnt;
+	uint16_t p_lvl2_lat, p_lvl3_lat, flush_size, flush_stride;
+	uint8_t duty_offset, duty_width, day_alrm, mon_alrm, century;
+	uint16_t iapc_boot_arch;
+	uint8_t reserved1;
+	uint32_t flags;
+	struct acpi_gas reset_reg;
+	uint8_t reset_value;
+	uint16_t arm_boot_arch;
+	uint8_t minor_version;
+	uint64_t x_firmware_ctrl;
+	uint64_t x_dsdt;
+	struct acpi_gas x_pm1a_evt_blk, x_pm1b_evt_blk, x_pm1a_cnt_blk, x_pm1b_cnt_blk;
+	struct acpi_gas x_pm2_cnt_blk, x_pm_tmr_blk, x_gpe0_blk, x_gpe1_blk;
+	struct acpi_gas sleep_control_reg, sleep_status_reg;
+	uint64_t hypervisor_vendor_id;
+} __attribute__ ((packed));
+
+struct ipl_xsdt {
+	struct acpi_sdt_header header;
+	uint64_t entries[2];		/* FADT, MADT */
+} __attribute__ ((packed));
+
+/* See: https://wiki.osdev.org/RSDP */
+struct XSDP {
+	char signature[8];
+	uint8_t checksum;
+	char oemid[6];
+	uint8_t revision;
+	uint32_t rsdtaddress;
+
+	uint32_t length;
+	uint64_t xsdtAddress;
+	uint8_t extendedchecksum;
+	uint8_t reserved[3];
+} __attribute__ ((packed));
+
 static struct ipl_madt madt = {
 	.header = {
-		.signature = "APIC",
-		.length = sizeof(struct ipl_madt),
-		.revision = 4,
-		.oem_id = "SMOLKV",
-		.oem_table_id = "SMOLMADT",
-		.oem_revision = 1,
-		.creator_revision = 1,
+		.signature = "APIC", .length = sizeof(struct ipl_madt), .revision = 4,
+		.oem_id = "SMOLKV", .oem_table_id = "SMOLMADT",
+		.oem_revision = 1, .creator_revision = 1,
 	},
 	.local_apic_address = (uint32_t) SMOLKVM_APIC_LAPIC_BASE,
 	.flags = 1,	/* PCAT_COMPAT: KVM's 8259 PICs are present */
-	.lapic = {
-		.type = 0, .length = 8,
-		.acpi_processor_id = 0, .apic_id = 0,
-		.flags = 1,	/* enabled */
-	},
+	.lapic = { .type = 0, .length = 8, .acpi_processor_id = 0, .apic_id = 0, .flags = 1 },
 	.ioapic = {
-		.type = 1, .length = 12,
-		.io_apic_id = 0,
+		.type = 1, .length = 12, .io_apic_id = 0,
 		.io_apic_address = (uint32_t) SMOLKVM_APIC_IOAPIC_BASE,
 		.gsi_base = SMOLKVM_APIC_IOAPIC_GSI_BASE,
 	},
 	/* Standard PC wiring: the PIT (ISA IRQ0) lands on IOAPIC pin 2 */
-	.pit_override = {
-		.type = 2, .length = 10,
-		.bus = 0, .source = 0, .gsi = 2, .flags = 0,
-	},
+	.pit_override = { .type = 2, .length = 10, .bus = 0, .source = 0, .gsi = 2, .flags = 0 },
 };
 
-static void acpi_set_checksum(struct acpi_sdt_header *h)
+static struct ipl_dsdt dsdt = {
+	.header = {
+		.signature = "DSDT", .length = sizeof(struct ipl_dsdt), .revision = 2,
+		.oem_id = "SMOLKV", .oem_table_id = "SMOLDSDT",
+		.oem_revision = 1, .creator_revision = 1,
+	},
+	.aml = { 0x10, 0x06, 0x5C, 0x5F, 0x53, 0x42, 0x5F },	/* Scope(\_SB){} */
+};
+
+static struct ipl_fadt fadt = {
+	.header = {
+		.signature = "FACP", .length = sizeof(struct ipl_fadt), .revision = 6,
+		.oem_id = "SMOLKV", .oem_table_id = "SMOLFADT",
+		.oem_revision = 1, .creator_revision = 1,
+	},
+	.flags = ACPI_FADT_HW_REDUCED,
+	.iapc_boot_arch = ACPI_IAPC_NO_VGA | ACPI_IAPC_NO_MSI,
+	.minor_version = 0,
+	/* dsdt / x_dsdt are filled at build time */
+};
+
+static struct ipl_xsdt xsdt = {
+	.header = {
+		.signature = "XSDT", .length = sizeof(struct ipl_xsdt), .revision = 1,
+		.oem_id = "SMOLKV", .oem_table_id = "SMOLXSDT",
+		.oem_revision = 1, .creator_revision = 1,
+	},
+	/* entries are filled at build time */
+};
+
+static struct XSDP rsdp = {
+	.signature = "RSD PTR ",
+	.oemid = "SMOLKV",
+	.revision = 2,
+	.rsdtaddress = 0,		/* we provide an XSDT, not an RSDT */
+	.length = sizeof(struct XSDP),
+	/* xsdtAddress and the checksums are filled at build time */
+};
+
+static uint8_t acpi_csum(const void *buf, uint32_t len)
 {
-	uint8_t *p = (uint8_t *) h;
+	const uint8_t *p = buf;
 	uint8_t sum = 0;
 	uint32_t i;
 
-	h->checksum = 0;
-	for (i = 0; i < h->length; i++)
+	for (i = 0; i < len; i++)
 		sum += p[i];
 
-	h->checksum = (uint8_t) (0 - sum);
+	return sum;
+}
+
+static void acpi_set_checksum(struct acpi_sdt_header *h)
+{
+	h->checksum = 0;
+	h->checksum = (uint8_t) (0 - acpi_csum(h, h->length));
 }
 
 static uint64_t build_madt(void)
 {
 	acpi_set_checksum(&madt.header);
-
 	return (uint64_t) (uintptr_t) &madt;
+}
+
+static uint64_t build_dsdt(void)
+{
+	acpi_set_checksum(&dsdt.header);
+	return (uint64_t) (uintptr_t) &dsdt;
+}
+
+static uint64_t build_fadt(uint64_t dsdt_addr)
+{
+	fadt.dsdt = (uint32_t) dsdt_addr;
+	fadt.x_dsdt = dsdt_addr;
+	acpi_set_checksum(&fadt.header);
+	return (uint64_t) (uintptr_t) &fadt;
+}
+
+static uint64_t build_xsdt(uint64_t fadt_addr, uint64_t madt_addr)
+{
+	xsdt.entries[0] = fadt_addr;
+	xsdt.entries[1] = madt_addr;
+	acpi_set_checksum(&xsdt.header);
+	return (uint64_t) (uintptr_t) &xsdt;
+}
+
+static uint64_t build_rsdp(uint64_t xsdt_addr)
+{
+	rsdp.xsdtAddress = xsdt_addr;
+
+	rsdp.checksum = 0;
+	rsdp.extendedchecksum = 0;
+	rsdp.checksum = (uint8_t) (0 - acpi_csum(&rsdp, 20));		/* ACPI 1.0 part */
+	rsdp.extendedchecksum = (uint8_t) (0 - acpi_csum(&rsdp, sizeof(rsdp)));
+
+	return (uint64_t) (uintptr_t) &rsdp;
 }
 
 /* --- boot ---------------------------------------------------------------- */
@@ -188,6 +306,16 @@ static void fill_boot_params(void)
 	bp->hdr.cmdline_size   = sizeof(cmdline);
 }
 
+static void build_acpi(void)
+{
+	uint64_t madt_addr = build_madt();
+	uint64_t dsdt_addr = build_dsdt();
+	uint64_t fadt_addr = build_fadt(dsdt_addr);
+	uint64_t xsdt_addr = build_xsdt(fadt_addr, madt_addr);
+
+	linux_boot_params.acpi_rsdp_addr = build_rsdp(xsdt_addr);
+}
+
 __attribute__((noreturn))
 static void jump_to_kernel(uint64_t entry, struct boot_params *params)
 {
@@ -231,7 +359,7 @@ void _c_start(void)
 	mailbox_post(SMOLKVM_MAILBOX_CMD_LOADKERNEL, &loadkernel);
 
 	printf("Building ACPI tables\n");
-	build_madt();
+	build_acpi();
 
 	printf("Filling boot params\n");
 	fill_boot_params();
