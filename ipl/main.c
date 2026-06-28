d39a34a39b49c61ae31b9b318d95ad6e9c7e528d
Daniel Palmer <daniel@thingy.jp>
2026-06-28 22:42:31 +0900

    tmp

--- ipl/main.c
+++ ipl/main.c
@@ -1,10 +1,48 @@
 #include <stdio.h>
+#include <asm/bootparam.h>
 
 #include "params.h"
 #include "mailbox.h"
 #include "loadkernel.h"
 
 static struct ipl_params ipl_params = { 0 };
+
+/* See: https://wiki.osdev.org/RSDP */
+struct XSDP {
+	char signature[8];
+	uint8_t checksum;
+	char oemid[6];
+	uint8_t revision;
+	uint32_t rsdtaddress;
+
+	uint32_t length;
+	uint64_t xsdtAddress;
+	uint8_t extendedchecksum;
+	uint8_t reserved[3];
+} __attribute__ ((packed));
+
+static struct XSDP rsdp = {
+}
+
+static struct boot_params linux_boot_params = { 0 };
+
+static void fill_boot_params(void)
+{
+
+}
+
+__attribute__((noreturn))
+static void jump_to_kernel(uint64_t entry, struct boot_params *params)
+{
+	__asm__ __volatile__(
+		"cli\n\t"
+		"jmp *%[entry]\n\t"
+		:
+		: [entry] "r"(entry), "S"(params)
+		: "memory");
+
+	__builtin_unreachable();
+}
 
 void _c_start(void)
 {
@@ -31,5 +69,5 @@ void _c_start(void)
 
 	printf("Jumping to entry @ %p\n", (void *) kernel_entry);
 
-	while(1) { };
+	jump_to_kernel(kernel_entry, &linux_boot_params);
 }
